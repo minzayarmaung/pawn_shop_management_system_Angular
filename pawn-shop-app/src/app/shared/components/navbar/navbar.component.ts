620eab4321245cf065f18717b8703934e867d0fd
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-08-09 19:59:04 +0630

    add

--- pawn-shop-app/src/app/shared/components/navbar/navbar.component.ts
+++ pawn-shop-app/src/app/shared/components/navbar/navbar.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule , FormsModule ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  
+  currentLanguage: string;
+  dropdownOpen = false;
+
+  changeLanguage(arg0: string) {
+  throw new Error('Method not implemented.');
+  }
+
+  constructor() {
+    this.currentLanguage = 'en';
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleDropdown() {
+      this.dropdownOpen = !this.dropdownOpen;
+  }
+
 }
