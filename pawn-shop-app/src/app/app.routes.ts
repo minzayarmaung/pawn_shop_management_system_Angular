cd82308138fb80e47aca3e057c01808e114e4dc6
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-08-15 21:00:15 +0630

    add configuration

--- pawn-shop-app/src/app/app.routes.ts
+++ pawn-shop-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { PawnItemsComponent } from './features/pawn-items/pawn-items.component';
+import { ConfigurationComponent } from './features/configuration/configuration.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'pawn-items', component: PawnItemsComponent },
+      { path: 'configuration', component: ConfigurationComponent },
     ]
   }
 ];
